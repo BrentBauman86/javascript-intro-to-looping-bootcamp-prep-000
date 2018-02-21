2cd9fa2ba89628a30a8663fa6b13701531cf3b5c
Brent Bauman <brentbauman86@gmail.com>
2018-02-21 00:06:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,3 +4,15 @@ for (let i = 0; i < 25; i++){
 }
 return array
 };
+
+function whileLoop(4){
+  while (i > 4)
+  console.log(--countdown)
+  return (`done`)
+};
+
+function doWhileLoop(array){
+function maybeTrue()
+do (array.length > && maybeTrue())
+return (array)
+};
